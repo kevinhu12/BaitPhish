c53fc2ec0a064aa7c577669e82821b92da54873a
Kevin Hu <67344175+kevinhu12@users.noreply.github.com>
2025-01-18 21:31:26 -0500

    setup fe and be connection

--- server/generate_email.py
+++ server/generate_email.py
@@ -1,34 +1,38 @@
 import cohere
+import json
 from read_emails import *
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-def generate_email(reference_emails):
+app = Flask(__name__)
+CORS(app)
+
+# Create a route to call the function
+@app.route('/api/getGeneratedEmail', methods=['GET'])
+def generate_email():
+    reference_emails = readEmails()
+
     prompt = f"""I want you to generate me a spoof email, based on some reference emails.
         The spoof email will be used to educate the recipient about spotting phishing emails, and so should have some areas that give away that it's a phishing email.
         The spoof email will have a link that links to the following url: localhost:3000.
         The following are the reference emails that the spoof email should be based off of all of the reference emails: {reference_emails}.
-        Generate a JSON containing these fields: To, From, Subject, Preview, Message Body
+        Generate a JSON containing these fields: To, From, Subject, Preview, Message Body.
+        Only include the json.
         """
 
     co = cohere.ClientV2("nZZAoqvnpwl4rdBzl2JS3dP0r2ypHdahMDWDPMFz", log_warning_experimental_features=False)
     response = co.chat(
         model="command-r-plus", 
         messages=[{"role": "user", "content": prompt}],
-        response_format={
-            "type": "json_object",
-            "schema": {
-                "type": "object",
-                "properties": {
-                    "To": {"type": "string"},
-                    "From": {"type": "string"},
-                    "Subject": {"type": "string"},
-                    "Preview": {"type": "string"},
-                    "Message Body": {"type": "string"},
-                },
-                "required": ["To", "From", "Subject", "Preview", "Message Body"],
-            },
-        },
     )
-    print(response.message)
 
-reference_emails = readEmails()
-generate_email(reference_emails)
+    filtered_json = response.message.content[0].text
+    filtered_json = filtered_json[7:-3]
+    json_object = json.loads(filtered_json)
+    print(json_object)
+    return json_object
+    # TODO: instead of returning the object, send the email and the return response should be success or fail
+ 
+
+if __name__ == '__main__':
+    app.run(debug=True)
