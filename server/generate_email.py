bb449073803f6e9b2f7ab477600cf8db31864cdc
ogtc20 <owengtchung@outlook.com>
2025-01-18 20:16:31 -0500

    Trying to get JSON formatting for generated email

--- server/generate_email.py
+++ server/generate_email.py
@@ -1,16 +1,34 @@
 import cohere
+from read_emails import *
 
-def generate_email(reference_emails, api_key):
-    prompt = """I want you to generate me a spoof email, based on some reference emails.
+def generate_email(reference_emails):
+    prompt = f"""I want you to generate me a spoof email, based on some reference emails.
         The spoof email will be used to educate the recipient about spotting phishing emails, and so should have some areas that give away that it's a phishing email.
         The spoof email will have a link that links to the following url: localhost:3000.
-        The following are the reference emails that the spoof email should be based off of: {reference_emails}.
+        The following are the reference emails that the spoof email should be based off of all of the reference emails: {reference_emails}.
+        Generate a JSON containing these fields: To, From, Subject, Preview, Message Body
         """
 
-    co = cohere.ClientV2(f"<<{api_key}>>")
+    co = cohere.ClientV2("nZZAoqvnpwl4rdBzl2JS3dP0r2ypHdahMDWDPMFz", log_warning_experimental_features=False)
     response = co.chat(
         model="command-r-plus", 
-        messages=[{"role": "user", "content": prompt}]
+        messages=[{"role": "user", "content": prompt}],
+        response_format={
+            "type": "json_object",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "To": {"type": "string"},
+                    "From": {"type": "string"},
+                    "Subject": {"type": "string"},
+                    "Preview": {"type": "string"},
+                    "Message Body": {"type": "string"},
+                },
+                "required": ["To", "From", "Subject", "Preview", "Message Body"],
+            },
+        },
     )
+    print(response.message)
 
-    print(response)
+reference_emails = readEmails()
+generate_email(reference_emails)
--- server/read_emails.py
+++ server/read_emails.py
@@ -1,28 +1,12 @@
 from simplegmail import Gmail
 
-gmail = Gmail()
-# Starred messages
-messages = gmail.get_starred_messages()
+def readEmails():
+  gmail = Gmail()
+  messages = gmail.get_starred_messages()
+  
+  reference_emails = []
+  for message in messages:
+    reference_emails.append(message.plain)
+  return reference_emails
 
-# ...and many more easy to use functions can be found in gmail.py!
-
-# Print them out!
-for message in messages:
-    print("To: " + message.recipient)
-    print("From: " + message.sender)
-    print("Subject: " + message.subject)
-    print("Date: " + message.date)
-    print("Preview: " + message.snippet)
-    print("Message Body: " + message.plain)  # or message.html
-
-from simplegmail import Gmail
-
-params = {
-  "to": "owengtchung@gmail.com",
-  "sender": "owengtchung@gmail.com",
-  "subject": "My first email",
-  "msg_html": "<h1>Woah, my first email!</h1><br />This is an HTML email.",
-  "msg_plain": "Hi\nThis is a plain text email.",
-}
-
-message = gmail.send_message(**params)  
+readEmails()
